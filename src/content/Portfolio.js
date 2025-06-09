9a5a33b47b82f94219354a958ab1b652c01220fa
md enamul <hfiroz1998@gmail.com>
2025-06-10 04:13:32 +0600

    update image

--- src/content/Portfolio.js
+++ src/content/Portfolio.js
@@ -1,42 +1,42 @@
 export const WebProject = [
     {
         "project_name": "Patient Management  ",
-        "project_type":"Full Stack Development",
+        "project_type": "Full Stack Development",
         "project_url": "https://doctorproject.searchfriend.pro/",
         "project_code": "",
         "screenshot": "/images/loremHomeo.webp"
     },
     {
         "project_name": "E - Commerce Web",
-        "project_type":"Full Stack Development",
+        "project_type": "Full Stack Development",
         "project_url": "https://doctorproject.searchfriend.pro/",
         "project_code": "",
         "screenshot": "/images/ecommerce-full.webp"
     },
     {
         "project_name": "E - Commerce Web",
-        "project_type":"UIUX Design",
+        "project_type": "UIUX Design",
         "project_url": "https://doctorproject.searchfriend.pro/",
         "project_code": "",
         "screenshot": "/images/ecommerce-ui.webp"
     },
     {
         "project_name": "E Learning Web",
-        "project_type":"Full Stack Development",
+        "project_type": "Full Stack Development",
         "project_url": "https://doctorproject.searchfriend.pro/",
         "project_code": "",
         "screenshot": "/images/apparel-full.webp"
     },
     {
         "project_name": "Freelancing Marketplace",
-        "project_type":"UIUX Design",
+        "project_type": "UIUX Design",
         "project_url": "https://doctorproject.searchfriend.pro/",
         "project_code": "",
         "screenshot": "/images/freelancer.webp"
     },
     {
         "project_name": "App Marketing ",
-        "project_type":"UIUX Design",
+        "project_type": "UIUX Design",
         "project_url": "https://doctorproject.searchfriend.pro/",
         "project_code": "",
         "screenshot": "/images/app-making.webp"
@@ -49,12 +49,39 @@ export const AppProject = [
 
     {
         "project_name": "E - Commerce App ",
-        "project_type":" UIUX Design",
+        "project_type": " UIUX Design",
         "project_url": "https://www.figma.com/proto/KSu53HtvBfgIJ2Ho3mBxAL/Ubizz---App-%2F-Web?page-id=2%3A1400&node-id=664-1373&viewport=1034%2C459%2C0.12&t=vxx9cf50RYURG3js-1&scaling=scale-down&content-scaling=fixed",
         "project_code": "",
         "screenshot": "/images/e-commerce-app.webp"
     },
-
+    {
+        "project_name": "Social Media",
+        "project_type": " UIUX Design",
+        "project_url": "https://www.figma.com/proto/KSu53HtvBfgIJ2Ho3mBxAL/Ubizz---App-%2F-Web?page-id=2%3A1400&node-id=664-1373&viewport=1034%2C459%2C0.12&t=vxx9cf50RYURG3js-1&scaling=scale-down&content-scaling=fixed",
+        "project_code": "",
+        "screenshot": "/images/e-commerce-app.webp"
+    },
+    {
+        "project_name": "E - Commerce App ",
+        "project_type": " UIUX Design",
+        "project_url": "https://www.figma.com/proto/KSu53HtvBfgIJ2Ho3mBxAL/Ubizz---App-%2F-Web?page-id=2%3A1400&node-id=664-1373&viewport=1034%2C459%2C0.12&t=vxx9cf50RYURG3js-1&scaling=scale-down&content-scaling=fixed",
+        "project_code": "",
+        "screenshot": "/images/e-commerce-app.webp"
+    },
+    {
+        "project_name": "E - Commerce App ",
+        "project_type": " UIUX Design",
+        "project_url": "https://www.figma.com/proto/KSu53HtvBfgIJ2Ho3mBxAL/Ubizz---App-%2F-Web?page-id=2%3A1400&node-id=664-1373&viewport=1034%2C459%2C0.12&t=vxx9cf50RYURG3js-1&scaling=scale-down&content-scaling=fixed",
+        "project_code": "",
+        "screenshot": "/images/e-commerce-app.webp"
+    },
+    {
+        "project_name": "E - Commerce App ",
+        "project_type": " UIUX Design",
+        "project_url": "https://www.figma.com/proto/KSu53HtvBfgIJ2Ho3mBxAL/Ubizz---App-%2F-Web?page-id=2%3A1400&node-id=664-1373&viewport=1034%2C459%2C0.12&t=vxx9cf50RYURG3js-1&scaling=scale-down&content-scaling=fixed",
+        "project_code": "",
+        "screenshot": "/images/e-commerce-app.webp"
+    }
 ]
 
 export const TemplateProject = [
@@ -62,7 +89,7 @@ export const TemplateProject = [
 
     {
         "project_name": "Portfolio Template ",
-        "project_type":" Figma To HTML",
+        "project_type": " Figma To HTML",
         "project_url": "https://enamul90.github.io/Multiple-Page-portfolio/index.html",
         "project_code": "",
         "screenshot": "/images/portfolio-project.webp"
@@ -74,7 +101,7 @@ export const BackendProject = [
 
     {
         "project_name": "Rest API Development",
-        "project_type":"Back Development",
+        "project_type": "Back Development",
         "project_url": "https://github.com/enamul90/Task_Manager_API_ES6",
         "project_code": "",
         "screenshot": "/images/rest-api-development.webp"
