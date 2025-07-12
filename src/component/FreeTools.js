824935c77396a32528df7ee966c92749e16e52ec
md enamul <hfiroz1998@gmail.com>
2025-07-13 03:38:19 +0600

    update code

--- src/component/FreeTools.js
+++ src/component/FreeTools.js
@@ -1,29 +1,27 @@
+import Link from 'next/link';
 import React from 'react';
 
 const FreeTools = () => {
     return (
         <div className =" px-5 mx-auto z-50 py-[150px] text-center ">
-            <button
-                
+            <Link  href={"https://unrivaled-profiterole-065686.netlify.app/"}
                 className="inline-flex  hover:border-b cursor-pointer items-center gap-1 px-3 py-1.5 text-lg font-medium text-white bg-secondary hover:bg-secondary/90 rounded-lg transition"
             >
                 Image To Text Converter
-            </button>
+            </Link>
 
-            <button
-                
+            <Link href={"https://dulcet-naiad-856776.netlify.app/"}
                 className="inline-flex hover:border-b cursor-pointer  items-center gap-1 px-3 py-1.5 text-lg font-medium text-white bg-secondary hover:bg-secondary/90 rounded-lg transition"
             >
 
                 Temp Mail
-            </button>
+            </Link>
 
-            <button
-                
+            <Link href={"https://beamish-cajeta-c8e24f.netlify.app/"}
                 className="inline-flex hover:border-b cursor-pointer  items-center gap-1 px-3 py-1.5 text-lg font-medium text-white bg-secondary hover:bg-secondary/90 rounded-lg transition"
             >
                 Image Formats Converter
-            </button>
+            </Link>
             
         </div>
     );
